d991bd2cc562fe7e8f947c5dfec6f12c7ccb10e0
Verti <verticix@gmail.com>
2026-05-19 23:16:31 -0700

    trying to understand ffmpeg without getting killed

--- main.gd
+++ main.gd
@@ -1,1 +1,7 @@
 extends Control
+
+func _ready() -> void:
+	var test = FFmpegTester.new()
+	print(test.get_ffmpeg_version())
+	print(test.is_ffmpeg_available())
+	print(test.get_libavcodec_version())
